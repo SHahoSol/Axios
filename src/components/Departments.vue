28ff20b8fbb1bb668f5fe7516b9811536632d05e
Shaho Soltani <soltani.shaho@gmail.com>
2018-08-23 22:43:29 +0300

    Added Timestamp to do comment

--- src/components/Departments.vue
+++ src/components/Departments.vue
@@ -2,13 +2,35 @@
 
   <div class="textBox">
     <h2 class="center">{{ msg }}</h2>
-    <textarea type="text" id="todoText" class="todoText" placeholder="Write" v-on:keyup="addTodo"/></textarea>
+    <textarea type="text"
+    id="todoText"
+    class="todoText"
+    placeholder="Write"
+    v-on:keyup="addTodo"/>
+    </textarea>
+
     <p class="center">Total Todo: {{ count }}</p>
     <ol>
       <li v-for="(todo, index) in todos">
         <div style="width:100%">
-          <div style="float:left;width:600px;border-radius: 5px;padding: 10px;margin: 10px;border-style: solid">{{ todo.title | uppercase }}</div>
-          <div style="float:left;width:1%;color:red;font-weight:bold;cursor:pointer" @click="deleteTodo(index)">X</div>
+          <div style="float:left;
+          width:600px;
+          border-radius: 5px;
+          padding: 10px;
+          margin: 10px;
+          border-style: solid">
+          {{ todo.title | uppercase }}
+          {{todo.timestamp | lowercase}}
+
+          </div>
+
+          <div style="float:left;
+          width:1%;
+          color:red;
+          font-weight:bold;
+          cursor:pointer"
+          @click="deleteTodo(index)">X</div>
+
           <div style="clear:both"></div>
         </div>
         </li>
@@ -17,38 +39,44 @@
 
 </template>
 
+
 //Vue.filter('uppercase', function(value){
   //return value.toUpperCase();
 //});
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   data() {
     return {
-      msg: 'Todo',
-    todos: []
-    }
+      msg: "Todo",
+      todos: [],
+      myDate: null
+    };
   },
   methods: {
-    addTodo: function(event){
-      if(event.keyCode==13 && event.target.value!=""){
-        this.todos.push({title:event.target.value,delete:false});
-        event.target.value = '';
+    addTodo(event) {
+      if (event.keyCode == 13 && event.target.value != "") {
+        let newDate = new Date();
+        this.todos.push({
+          title: event.target.value,
+          delete: false,
+          timestamp: newDate.toUTCString()
+        });
+        event.target.value = "";
       }
     },
-    deleteTodo: function(index){
-      this.todos.splice(index,1);
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
     }
   },
   computed: {
-    count: function(){
+    count() {
       return this.todos.length;
     }
   }
-}
+};
 </script>
 
 <style scoped>
-
 </style>
